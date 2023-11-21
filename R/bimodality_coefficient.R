22bafcfcf7b2e74e4acef5a79efd84e67db902b2
Chris McGinnis <cmcginni92@gmail.com>
2023-11-21 15:19:05 -0500

    Delete R directory

--- R/bimodality_coefficient.R
+++
@@ -1,8 +0,0 @@
-bimodality_coefficient <- function(x) {
-  G <- skewness(x)
-  sample.excess.kurtosis <- kurtosis(x)
-  K <- sample.excess.kurtosis
-  n <- length(x)
-  B <- ((G^2)+1)/(K+((3*((n-1)^2))/((n-2)*(n-3))))
-  return(B)
-}
--- R/modelHomotypic.R
+++
@@ -1,5 +0,0 @@
-modelHomotypic <- function(annotations) {
-  anno.freq <- table(annotations)/length(annotations)
-  x <- sum(anno.freq^2)
-  return(x)
-}
