5f221d394186c23027b01d9f96902799c59f37c4
Seshagiri Nuthalapati <nasgrao@gmail.com>
2024-05-11 18:02:20 -0700

    fixed variable name

--- R/doubletFinder.R
+++ R/doubletFinder.R
@@ -25,9 +25,9 @@ doubletFinder <- function(object, PCs, pN = 0.25, pK, nExp, reuse.pANN = FALSE, 
   #   colnames(doublets) <- paste("X", 1:n_doublets, sep = "")
   #   data_wdoublets <- cbind(data, doublets)
 
-    data <- seu@assays$RNA@layers$counts ## M
-    colnames(data) <- colnames(seu) ## M
-    rownames(data) <- rownames(seu) ## M
+    data <- object@assays$RNA@layers$counts ## M
+    colnames(data) <- colnames(object) ## M
+    rownames(data) <- rownames(object) ## M
     data <- data[, real.cells] ## M
 
   ## Make merged real-artifical data
