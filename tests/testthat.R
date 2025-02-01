e528fffcf543fde1e7d49b990fd846b54d228db9
Liu <haibo.liu@umassmed.edu>
2025-01-31 21:23:40 -0500

    update DESCRIPTION

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,5 @@
 library(testthat)
 library(DoubletFinder)
+library("Seurat")
 
 test_check("DoubletFinder")
--- tests/testthat/test-doubletFinder_v3.R
+++ tests/testthat/test-doubletFinder_v3.R
@@ -1,26 +1,26 @@
-test_that("doubletFinder_v3 works", {
-  library("Seurat")
+test_that("doubletFinder works", {
+
   # Make test deterministic
   set.seed(0)
   # Err if wrong length, type, or if any missingness
   testthat::expect_error(
-    doubletFinder_v3(pbmc_small, PCs = 1:4, pN = 0.25, pK = 0.01, nExp = 5, reuse.pANN = FALSE, sct=FALSE,
+    doubletFinder(pbmc_small, PCs = 1:4, pN = 0.25, pK = 0.01, nExp = 5, reuse.pANN = FALSE, sct=FALSE,
                      annotations = "blah")
   )
   testthat::expect_error(
-    doubletFinder_v3(pbmc_small, PCs = 1:4, pN = 0.25, pK = 0.01, nExp = 5, reuse.pANN = FALSE, sct=FALSE,
+    doubletFinder(pbmc_small, PCs = 1:4, pN = 0.25, pK = 0.01, nExp = 5, reuse.pANN = FALSE, sct=FALSE,
                      annotations = as.factor(pbmc_small$RNA_snn_res.1))
   )
   testthat::expect_error(
-    doubletFinder_v3(pbmc_small, PCs = 1:4, pN = 0.25, pK = 0.01, nExp = 5, reuse.pANN = FALSE, sct=FALSE,
+    doubletFinder(pbmc_small, PCs = 1:4, pN = 0.25, pK = 0.01, nExp = 5, reuse.pANN = FALSE, sct=FALSE,
                      annotations = as.character(NA*as.numeric(pbmc_small$RNA_snn_res.1)))
   )
   testthat::expect_output(
-    pbmc_small_dubs <- doubletFinder_v3(pbmc_small, PCs = 1:4, pN = 0.25, pK = 0.01, nExp = 5, reuse.pANN = FALSE, sct=FALSE,
+    pbmc_small_dubs <- doubletFinder(pbmc_small, PCs = 1:4, pN = 0.25, pK = 0.01, nExp = 5, reuse.pANN = FALSE, sct=FALSE,
                                         annotations = as.character(pbmc_small$RNA_snn_res.1))
   )
   testthat::expect_output(
-    pbmc_small_dubs <- doubletFinder_v3(pbmc_small, PCs = 1:4, pN = 0.5, pK = 0.01, nExp = 5, reuse.pANN = FALSE, sct=FALSE,
+    pbmc_small_dubs <- doubletFinder(pbmc_small, PCs = 1:4, pN = 0.5, pK = 0.01, nExp = 5, reuse.pANN = FALSE, sct=FALSE,
                                         annotations = as.character(pbmc_small$RNA_snn_res.1))
   )
   testthat::expect_length( pbmc_small_dubs$DF.doublet.contributors_0.5_0.01_5_0, 80 )
